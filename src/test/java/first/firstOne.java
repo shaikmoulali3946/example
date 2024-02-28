f32e83f3d346785a1a52b3aa3cd2506aa77915a6
moulali <moulalishaik3946@gmail.com>
2024-02-28 16:26:01 +0530

    My second Commit

--- src/test/java/first/firstOne.java
+++ src/test/java/first/firstOne.java
@@ -1,6 +1,7 @@
 package first;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -8,13 +9,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
 
-public class firstOne {
+public class firstOne{
 	WebDriver driver;
 	@Test
 	void mm()
 	{
-		WebDriverManager.firefoxdriver().setup();
-		driver=new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.facebook.com/");
